9b765775e2efb641cbbe46a34d31b1775407bbca
andy myers <myersans@iu.edu>
2021-12-09 18:42:50 -0500

    commit

--- 02-Save/Player/Player_Container.gd
+++ 02-Save/Player/Player_Container.gd
@@ -4,8 +4,13 @@ onready var Player = load("res://Player/Player.tscn")
 var starting_position = Vector2(200,200)
 
 
+func _ready():
+	pass
+
+
 func _physics_process(_delta):
 	if not has_node("Player"):
 		var player = Player.instance()
 		player.position = starting_position
 		add_child(player)
+		player.get_node("Camera2D").current = true
